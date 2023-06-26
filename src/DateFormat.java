28009254bbfe207411478489fbdaac6edf33c351
anastasiya.popkova <anastasiya.buyko.teachmeskills@gmail.com>
2023-06-26 22:15:57 +0300

    fix 26/06/2023

--- src/DateFormat.java
+++ src/DateFormat.java
@@ -21,7 +21,6 @@ public class DateFormat {
             years++;
         }
         else {
-            flag = false;
             if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && day == 31) {
                 month++;
                 day = 1;
