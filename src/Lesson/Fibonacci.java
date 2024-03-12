0c2a91c85b0b959244d45974dc21519f39b00ef1
buykoam <anastasiya.buyko.teachmeskills@gmail.com>
2024-03-12 20:26:45 +0300

    12.03.24

--- src/Lesson/Fibonacci.java
+++ src/Lesson/Fibonacci.java
@@ -1,2 +1,21 @@
-package Lesson;public class Fibonacci {
+package Lesson;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+
+        int fib1 = 1;
+        int fib2 = 1;
+        int fib_sum;
+        System.out.print(fib1 + " " + fib2 + " ");
+
+        for (int i = 3; i <= 11; i++) {
+
+            fib_sum = fib1 + fib2;
+            System.out.print(fib_sum + " ");
+            fib1 = fib2;
+            fib2 = fib_sum;
+        }
+    }
 }
--- src/Lesson/SumNumbers.java
+++ src/Lesson/SumNumbers.java
@@ -3,6 +3,7 @@ package Lesson;
 import java.util.Scanner;
 
 public class SumNumbers {
+    //Написать программу, вычисляющую сумму цифр введённого пользователем целого числа.
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите число:");
