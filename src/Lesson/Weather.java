1b4be0fd8a33f22e5bda333afca185b741e76023
buykoam <anastasiya.buyko.teachmeskills@gmail.com>
2024-03-30 20:59:36 +0300

    30.03.2024

--- src/Lesson/Weather.java
+++ src/Lesson/Weather.java
@@ -1,4 +1,25 @@
 package Lesson;
 
-public class Wether {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Weather {
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        System.out.println("Введите количество дней:");
+
+        int number = n.nextInt();
+        for(int i = 1; i <= number;  i++) {
+            Scanner a = new Scanner(System.in);
+            System.out.println("Введите количество осадков за день:");
+            int ocadki = a.nextInt();
+
+        }
+
+
+        System.out.println("Спасибо! Вы ввели число " + number);
+
+
+    }
 }
