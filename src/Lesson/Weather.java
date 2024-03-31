3239b52e5b1f91fa4f15a977657466d4a52887f4
buykoam <anastasiya.buyko.teachmeskills@gmail.com>
2024-03-31 12:13:41 +0300

    somthing fixed 31/03/24

--- src/Lesson/Weather.java
+++ src/Lesson/Weather.java
@@ -3,6 +3,7 @@ package Lesson;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Weather {
     public static void main(String[] args) {
@@ -10,16 +11,18 @@ public class Weather {
         System.out.println("Введите количество дней:");
 
         int number = n.nextInt();
-        for(int i = 1; i <= number;  i++) {
+        ArrayList<String> list = new ArrayList<>();
+        list.addAll(list);
+        int ocadki = 0;
+        for (int i = 1; i <= number; i++) {
             Scanner a = new Scanner(System.in);
             System.out.println("Введите количество осадков за день:");
-            int ocadki = a.nextInt();
+            ocadki = a.nextInt();
+            list.add(String.valueOf(ocadki));
 
         }
-
-
-        System.out.println("Спасибо! Вы ввели число " + number);
-
-
+        int total = IntStream.of(list).sum();
+        System.out.println("Количество дней" + number);
+        System.out.println("Количество осдаков" + total);
     }
 }
